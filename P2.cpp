d8a7625ae40bdca16e5d87ed5cfa86d7dfbcbf98
Varnika-06 <varnikastuff234@gmail.com>
2025-07-22 14:05:17 +0530

    Create P2.cpp

---
+++ P2.cpp
@@ -0,0 +1,25 @@
+//NAME: Varnika Maurya
+//PRN: 24070123160
+//Exp-5
+//Program 1
+#include <iostream>
+using namespace std;
+//Program 2
+int main() {
+    char ch;
+    cout << "Enter a character: ";
+    cin >> ch;
+    if(ch=='A' || ch=='E' || ch=='I' || ch=='O' || ch=='U' ||
+       ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') {
+        cout << ch << " is a vowel." << endl;
+    } else {
+        cout << ch << " is a consonant." << endl;
+    }   
+    return 0;
+}
+/*
+PS C:\Users\DELL\Desktop\C++> g++ P2.cpp -o P2
+PS C:\Users\DELL\Desktop\C++> .\P2.exe        
+Enter a character: A
+A is a vowel.
+  */
